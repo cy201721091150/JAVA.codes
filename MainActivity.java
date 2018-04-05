d55c8b13ad91cf67b35dd989ff394ee6ea77b3e3
cy201721091150 <37445384+cy201721091150@users.noreply.github.com>
2018-04-05 12:43:56 +0800

    ActivityLifeCycleTest

--- MainActivity.java
+++ MainActivity.java
@@ -1,77 +1,76 @@
-package com.example.a24270.uidesign;
+package com.example.a24270.activitylifecycletest;
 
-import android.app.ProgressDialog;
-import android.content.DialogInterface;
-import android.support.v7.app.AlertDialog;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.Editable;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.ProgressBar;
-import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    private EditText editText;
-    private ImageView imageView;
-    private ProgressBar progressBar;
+public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG="MainActivity";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button button=(Button) findViewById(R.id.button );
-        editText=(EditText)findViewById(R.id.edit_text);
-        imageView=(ImageView)findViewById(R.id.image_view);
-        progressBar=(ProgressBar)findViewById(R.id.progress_bar);
-        button.setOnClickListener(this);}
-    @Override
-            public void onClick(View v){
-                switch (v.getId()){
-                    case R.id.button:
-                        ProgressDialog progressDialog=new ProgressDialog(MainActivity.this);
-                        progressDialog.setTitle("This is ProgressDialog");
-                        progressDialog.setMessage("等会嘛!!!!");
-                        progressDialog.setCancelable(true);
-                        progressDialog.show();
-                                break;
-                            default:
-                                break;
-                            /* 1.显示浮窗Toast.makeText(MainActivity.this,inputText,Toast.LENGTH_SHORT).show();
-                                2.显示图片imageView.setImageResource(R.drawable.img_2);
-                                3.进度条在可见和不可见之间循环
-                                case R.id.button:
-                                    if (progressBar.getVisibility()==View.GONE){
-                                        progressBar.setVisibility(View.VISIBLE);}
-                                    else{
-                                        progressBar.setVisibility(View.GONE);
-                                            }
-                                 4.水平进度条
-                                 int progress=progressBar.getProgress();
-                                    progress=progress+10;
-                                        progressBar.setProgress(progress);
-                                  5.对话框
-                                  AlertDialog.Builder dialog=new AlertDialog.Builder(MainActivity.this);
-                        dialog.setTitle("This id Dialog");
-                        dialog.setMessage("Something important");
-                        dialog.setCancelable(true);//退出时能不能使用back键退出
-                        dialog.setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialogInterface, int which) {
-
-                            }
-                        });
-                        dialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialogInterface, int which) {
-
-                            }
-                        });dialog.show();
-
-                                 */
-
-
+        if (savedInstanceState!=null){
+            String tempData=savedInstanceState.getString("data_key");
+            Log.d(TAG,tempData);
+        }
+        Button startNormalActivity=(Button) findViewById(R.id.start_normal_activity);
+        Button startDialogActivity=(Button) findViewById(R.id.start_dialog_activity);
+        startNormalActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(MainActivity.this,NormalActivity.class);
+                startActivity(intent);
             }
+        });
+        startDialogActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(MainActivity.this,DialogActivity.class);
+                startActivity(intent);
+            }
+    });
+}
+    @Override
+    protected void onSaveInstanceState(Bundle outState){
+        super.onSaveInstanceState(outState);
+        String tempData="Something you just typed";
+        outState.putString("data_key",tempData);
+    }
+@Override
+protected void onStart(){
+    super.onStart();
+    Log.d(TAG,"onStart");
+    }
+    @Override
+    protected void onResume(){
+        super.onResume();
+        Log.d(TAG,"onResume");
+    }
+    @Override
+    protected void onPause(){
+        super.onPause();
+        Log.d(TAG,"onPause");
+    }
+    @Override
+    protected void onStop(){
+        super.onStop();
+        Log.d(TAG,"onStop");
+    }
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        Log.d(TAG,"onDestroy");
+    }
+    @Override
+    protected void onRestart(){
+        super.onRestart();
+        Log.d(TAG,"onRestart");
     }
 }
+
